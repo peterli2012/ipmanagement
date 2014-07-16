3c279fac8e374c05b1ddf63cc6052cd0d96214e3
pli <pli@LIMLI-CN>
2014-07-16 17:14:43 +0800

    commit

--- src/bl/beans/IpTagBean.java
+++ src/bl/beans/IpTagBean.java
@@ -1,5 +1,41 @@
 package bl.beans;
 
 public class IpTagBean extends Bean {
+    private Integer tag;
+    private String ip;
+    private Integer subnet;
+    private String comments;
+
+    public Integer getTag() {
+        return tag;
+    }
+
+    public void setTag(Integer tag) {
+        this.tag = tag;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getSubnet() {
+        return subnet;
+    }
+
+    public void setSubnet(Integer subnet) {
+        this.subnet = subnet;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 
 }
